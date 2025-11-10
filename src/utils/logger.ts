b721af0661769ae9fb694cc531f76af65ca471cc
markedFisher <markfisher7797@iejesusmirey.com>
2025-11-10 21:17:34 +0430

    feat: add src/utils/logger.ts

---
+++ src/utils/logger.ts
@@ -0,0 +1,12 @@
+import pino from "pino";
+
+export function createLogger() {
+return pino({
+level: process.env.LOG_LEVEL || "info",
+transport:
+process.env.NODE_ENV !== "production"
+? { target: "pino-pretty", options: { colorize: true } }
+: undefined,
+base: undefined
+});
+}
